5bdba8262946580922812f91a6db2854a5017ee6
Mihai Mocanu <mvmocanu@users.noreply.github.com>
2015-05-27 10:32:49 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
 
-from distutils.core import setup
-
-setup(name='zencoder',
-      version='0.4',
-      description='Integration library for Zencoder',
-      author='Alex Schworer',
-      author_email='alex.schworer@gmail.com',
-      url='http://github.com/schworer/zencoder-py',
-      license="MIT License",
-      install_requires=['httplib2'],
-      packages=['zencoder']
-     )
-
+setup(
+    name='zencoder',
+    version='0.4.1',
+    description='Integration library for Zencoder',
+    author='Alex Schworer',
+    author_email='alex.schworer@gmail.com',
+    url='http://github.com/schworer/zencoder-py',
+    license="MIT License",
+    install_requires=['httplib2'],
+    packages=find_packages('zencoder'),
+)
