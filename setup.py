8c5c1a4e8621f4730bf1726514b4b3033a0a4ce5
Mihai Mocanu <mvmocanu@users.noreply.github.com>
2015-05-27 13:17:25 +0300

    Updated zencoder.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='zencoder',
+    name='pbs-zencoder',
     version='0.4.1',
     description='Integration library for Zencoder',
     author='Alex Schworer',
@@ -10,6 +10,6 @@ setup(
     url='http://github.com/schworer/zencoder-py',
     license="MIT License",
     install_requires=['httplib2'],
-    packages=find_packages('zencoder'),
+    packages=find_packages(),
     include_package_data=True,
 )
